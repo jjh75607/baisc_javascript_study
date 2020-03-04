100923486b5e8468f0e38aa2fc146ef015d9d135
jjh75607 <dkrak5738@naver.com>
2020-03-04 18:38:02 +0900

    gugudan page

--- gugudan/gugudan.js
+++ gugudan/gugudan.js
@@ -1,21 +1,41 @@
-while(true) {
-    var n1 = Math.floor(Math.random() * 9) + 1;
-    var n2 = Math.floor(Math.random() * 9) + 1;
-    var result = n1 * n2;
+var n1 = Math.ceil(Math.random() * 9);
+var n2 = Math.ceil(Math.random() * 9);
+var result = n1 * n2;
 
-    var answer = Number(prompt(`${n1} * ${n2}`));
-    var isCheck = true;
+var body = document.body;
+var word = document.createElement('div');
+word.textContent = `${n1} * ${n2}`;
+body.append(word);
 
-    if(answer === 0) {
-        break;
+var form = document.createElement('form');
+body.append(form);
+
+var answer = document.createElement('input');
+form.append(answer);
+answer.focus();
+
+var button = document.createElement('button');
+button.textContent = '입력';
+form.append(button);
+
+var resultText = document.createElement('div');
+body.append(resultText);
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    if(result === Number(answer.value)) {
+        resultText.textContent = "success";
+        answer.value = '';
+        answer.focus();
+
+        n1 = Math.ceil(Math.random() * 9);
+        n2 = Math.ceil(Math.random() * 9);
+        result = n1 * n2;
+        word.textContent = `${n1} * ${n2}`;
+    } else {
+        resultText.textContent = "error";
+        answer.value = '';
+        answer.focus();
     }
-
-    while(isCheck) {
-        if(answer === result) {
-            alert("O");
-            isCheck = false;
-        } else {
-            alert("X");
-        }
-    }
-}
+});
