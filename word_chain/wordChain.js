b0c26ecf3c638cd666b1a198bc7c343e9a7ca685
jjh75607 <dkrak5738@naver.com>
2020-03-04 17:45:14 +0900

    wordChain page

--- word_chain/wordChain.js
+++ word_chain/wordChain.js
@@ -1,21 +1,41 @@
-var word = "시작";
+var body = document.body;
+var word = document.createElement('div');
+word.textContent = '시작';
+body.append(word);
 
-while(true) {
-    var answer = prompt(word);
+var form = document.createElement('form');
+body.append(form);
 
-    if(answer === "끝") {
-        break;
+var answer = document.createElement('input');
+form.append(answer);
+answer.focus();
+
+var button = document.createElement('button');
+button.textContent = '입력';
+form.append(button);
+
+var result = document.createElement('div');
+body.append(result);
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    if(answer.value.length === 1) {
+        result.textContent = "2자리이상";
+        answer.value = '';
+        answer.focus();
+        
+        return 0;
     }
 
-    if(answer.length === 1) {
-        alert("1글자 이상 입력");
-        continue;
-    }
-
-    if(word[word.length - 1] === answer[0]) {
-        word = answer;
-        alert("정답");
+    if(word.textContent[word.textContent.length - 1] === answer.value[0]) {
+        result.textContent = 'success';
+        word.textContent = answer.value;
+        answer.value = '';
+        answer.focus();
     } else {
-        alert("오답");
+        result.textContent = 'error';
+        answer.value = '';
+        answer.focus();
     }
-}
+});
