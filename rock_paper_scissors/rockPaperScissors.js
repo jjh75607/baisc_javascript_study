e632f48b2f0536a9031004c595c50fe589f5268c
jjh75607 <dkrak5738@naver.com>
2020-03-10 22:11:15 +0900

    가위바위보

--- rock_paper_scissors/rockPaperScissors.js
+++ rock_paper_scissors/rockPaperScissors.js
@@ -1,26 +1,57 @@
-var computer = 0;
+var imgCoor = 0;
 var rockPaperScissors = {
     바위: '0',
     가위: '-142px',
     보: '-284px'
 };
 
-setInterval(() => {
-    if (computer === rockPaperScissors.바위) {
-        computer = rockPaperScissors.가위;
-    } else if (computer === rockPaperScissors.가위) {
-        computer = rockPaperScissors.보;
-    } else {
-        computer = rockPaperScissors.바위;
-    }
+function computerChoice(imgCoor) {
+    return Object.entries(rockPaperScissors).find((y) => {
+        return y[1] === imgCoor;
+    })[0];
+};
 
-    document.querySelector('#computer').style.background =
-        'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + computer + ' 0';
-}, 100);
+var interval;
+function intervalMaker() {
+    interval = setInterval(() => {
+        if (imgCoor === rockPaperScissors.바위) {
+            imgCoor = rockPaperScissors.가위;
+        } else if (imgCoor === rockPaperScissors.가위) {
+            imgCoor = rockPaperScissors.보;
+        } else {
+            imgCoor = rockPaperScissors.바위;
+        }
 
+        document.querySelector('#computer').style.background =
+            'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + imgCoor + ' 0';
+    }, 100);
+}
 
-document.querySelectorAll('.btn').forEach(function (btn) {
-    btn.addEventListener('click', function () {
-        console.log(btn.textContent);
+intervalMaker();
+
+var scoreBoard = {
+    가위: 1,
+    바위: 0,
+    보: -1
+};
+
+document.querySelectorAll('.btn').forEach((btn) => {
+    btn.addEventListener('click', () => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            intervalMaker();
+        }, 1000);
+
+        var myChoice = btn.textContent;
+        var score = scoreBoard[myChoice] - scoreBoard[computerChoice(imgCoor)];
+
+        if (score === 0) {
+            console.log('a draw');
+        } else if ([-1, 2].includes(score)) {
+            console.log('victory');
+        } else {
+            console.log('defeat');
+        }
     });
 });
